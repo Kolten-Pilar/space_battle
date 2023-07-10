eddfc770d71be16e0c360187307381a606ab61e5
Kolten ✔️ <kolten.pilar@gmail.com>
2023-07-10 13:48:33 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// create ship class
+class Ship {
+  constructor(name, hull, firepower, accuracy) {
+    this.name = name
+    this.hull = hull
+    this.firepower = firepower
+    this.accuracy = accuracy
+  }
+}
+
