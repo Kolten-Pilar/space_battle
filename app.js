888dff72195fa2ff3d8c135f75a6c67f38ff3343
Kolten ✔️ <kolten.pilar@gmail.com>
2023-07-10 14:27:09 -0400

    created player and alien ships

--- app.js
+++ app.js
@@ -8,3 +8,22 @@ class Ship {
   }
 }
 
+// player spaceship
+let playerShip = new Ship ('USS Assembly', 20, 5, 0.7)
+
+// create function to pick random number between given numbers in whole numbers
+const randomWholeNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// create function to pick random number between given numbers in decimal
+const randomDecimalNumber = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+
+// alien spaceship
+let alienShip = new Ship ('Bad Boy', randomWholeNumber(3, 6),randomWholeNumber(2, 4), randomDecimalNumber(0.6, 0.8), )
+
+
